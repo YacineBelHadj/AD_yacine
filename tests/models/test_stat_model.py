e2d4aa84539c7590a2a0975a8370aa89dbed396c
Yacine Bel-Hadj <yacine.bel-hadj@vub.be>
2022-12-12 14:08:55 +0100

    new: prepare dataset

--- tests/models/test_stat_model.py
+++ tests/models/test_stat_model.py
@@ -1,9 +1,39 @@
-from AD_vibration.models import StatModel
+import unittest
+import numpy as np
+import pandas as pd
+from sklearn.datasets import make_classification
+from AD_vibration.models.stat_model import MahalanobisDistance
+class MahalanobisDistanceTestCase(unittest.TestCase):
+    def test_mahalanobis_distance(self):
+        # Create some test data
+        X, _ = make_classification(n_samples=100, n_features=10, random_state=42)
 
-def test_Mahalanobis():
-    # Test Mahalanobis distance calculation
-    x = np.array([1, 2, 3, 4, 5])
-    y = np.array([2, 4, 6, 8, 10])
-    z = np.array([1, 2, 3, 4, 5])
-    assert StatModel.Mahalanobis(x, y) == 2.0
-    assert StatModel.Mahalanobis(x, z) == 0.0
+        # Create a MahalanobisDistance instance
+        m = MahalanobisDistance()
+
+        # Fit the model to the test data
+        m.fit(X)
+
+        # Compute the Mahalanobis distance for the test data
+        distance = m.transform(X)
+        print(distance.shape)
+
+        # Assert that the shape of the distance is as expected
+        self.assertEqual(distance.shape, (100,))
+
+        # Assert that the distance is greater than zero for all samples
+        self.assertTrue(np.all(distance > 0))
+
+        # Compute the Mahalanobis distance for a DataFrame of test data
+        df = pd.DataFrame(X)
+        distance = m.transform(df)
+
+        # Assert that the shape of the distance is as expected
+        self.assertEqual(distance.shape, (100,))
+
+        # Assert that the distance is greater than zero for all samples
+        self.assertTrue(np.all(distance > 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
